8798c3a4f02a5238c66a51aa35f3188fc8faa621
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-07-17 22:45:43 +0100

    Add project files.

---
+++ ZakaBankLogicLayer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZakaBankLogicLayer
+{
+    public class Class1
+    {
+    }
+}
